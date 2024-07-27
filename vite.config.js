3ff9818dc269fb061adba0aedf2ad2ab52dcf672
Dheemanth Sai Majji <dheemanth.s.majji@vanderbilt.edu>
2024-07-27 17:08:50 -0600

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base : "/dmajji.github.io/",
-  plugins : [react()], 
-})
+  base: "/dmajji.github.io/",
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/3d/*.glb',
+          dest: 'assets/3d'
+        }
+      ]
+    })
+  ]
+});
